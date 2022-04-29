deef916b35fe961feaae9a9a1be08d156982566e
soniasltan <sonnyeah@gmail.com>
2022-04-29 11:53:07 +0800

    added Project Management solution

---
+++ ProjectManagement/ProjectManagement/Models/UserList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectManagement.Models
+{
+    public partial class UserList
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public int? UserRolesId { get; set; }
+    }
+}
